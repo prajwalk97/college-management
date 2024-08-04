75884354f6a3bb9e77f53e229be62306ebaceec4
Prajwal K <prajwalindia18@gmail.com>
2024-08-04 20:33:09 +0530

    added env to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 import Router from './Router';
 import { createTheme, ThemeProvider } from '@mui/material';
-axios.defaults.baseURL = "http://localhost:1500/api/";
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL ?? "http://localhost:1500" + "/api/";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem(
   "x-auth-token"
 );
 const App = () => {
+  console.log(import.meta.env.VITE_SERVER_URL);
   const appTheme = createTheme({
     zIndex: {
       drawer: 1200
