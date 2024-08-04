2bd47b65b57e6300e27c329945f6366767e84c3a
Prajwal K <prajwalindia18@gmail.com>
2024-08-04 20:21:08 +0530

    added db.js

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -2,11 +2,12 @@ const mysql = require("mysql2")
 
 
 const db = mysql.createPool({
-    host: "0.0.0.0",
-    user: "root",
-    password: "my-secret-pw",
-    database: "collegeSpace",
-    port: "3306"
+    host: process.env.host ?? "0.0.0.0",
+    user: process.env.user ?? "root",
+    password: process.env.password ?? "my-secret-pw",
+    database: process.env.database ?? "collegeSpace",
+    port: process.env.port ?? "3306"
 }).promise();
 
 module.exports = db
+
